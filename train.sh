eb37f58d8c9c3931553d8d386c0dba8605f47c14
AdamBear <adamxiong@126.com>
2022-06-29 15:37:37 +0800

    add train bash script

---
+++ train.sh
@@ -0,0 +1,1 @@
+python train.py -p config/pretrain/preprocess.yaml -m config/pretrain/model.yaml -t config/pretrain/train.yaml --vocoder_checkpoint vocoder/g_hifigan.pt --vocoder_config vocoder/config/config_16k.json 
